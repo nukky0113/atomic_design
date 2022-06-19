f2806e1a0bdcdbc6b87a826c527af376465c91ec
nukky0113 <56378055+nukky0113@users.noreply.github.com>
2022-06-19 16:13:02 +0900

    Initial commit

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SContanier>
+      <h2>Topページです</h2>
+    </SContanier>
+  );
+};
+
+const SContanier = styled.div`
+  text-align: center;
+`;
